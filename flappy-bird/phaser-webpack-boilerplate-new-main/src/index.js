8ad3949367d31c0a9f305fc628ac27809cd48bd9
dhk939393 <dhk9309@gmail.com>
2024-12-04 07:28:57 +0900

    물리 시험중

--- flappy-bird/phaser-webpack-boilerplate-new-main/src/index.js
+++ flappy-bird/phaser-webpack-boilerplate-new-main/src/index.js
@@ -5,7 +5,7 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  Physics: {
+  physics: {
     default: 'arcade'
   },
   scene: {
@@ -19,11 +19,22 @@ function preload() {
   this.load.image('bird', 'assets/bird.png');
 }
 
+let bird = null;
+
 function create() {
   // this.add.image(config.width / 2, config.height / 2, 'sky');
-  this.add.image(0, 0, 'sky').setOrigin(0)
+  this.add.image(0, 0, 'sky').setOrigin(0);
 
-  this.add.sprite(config.width / 2, config.height / 2, 'bird' ).setOrigin(0)
+  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0);
+  // bird.body.gravity.y = 200;
+  bird.body.velocity.y = 200;
+}
+
+// 60fps
+// 60 times 
+function update(time, delta) {
+  console.log(bird.body.velocity.y)
+
 }
 
 
