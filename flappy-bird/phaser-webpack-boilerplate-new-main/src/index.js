af8bc3db97d15c2b6f33a7d33ffe61315d2b0050
dhk939393 <dhk9309@gmail.com>
2024-12-06 00:21:21 +0900

    중력 테스트

--- flappy-bird/phaser-webpack-boilerplate-new-main/src/index.js
+++ flappy-bird/phaser-webpack-boilerplate-new-main/src/index.js
@@ -6,11 +6,16 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade'
+    default: 'arcade',
+    arcade: {
+      // gravity: { y: 200 }
+      debug: true
+    }
   },
   scene: {
     preload,
     create,
+    update
   }
 }
 
@@ -19,21 +24,28 @@ function preload() {
   this.load.image('bird', 'assets/bird.png');
 }
 
+const VELOCITY = 200;
+
 let bird = null;
+let totalDelta = null;
 
 function create() {
-  // this.add.image(config.width / 2, config.height / 2, 'sky');
   this.add.image(0, 0, 'sky').setOrigin(0);
 
   bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0);
-  // bird.body.gravity.y = 200;
-  bird.body.velocity.y = 200;
+  bird.body.velocity.x = VELOCITY;
 }
 
 // 60fps
 // 60 times 
 function update(time, delta) {
-  console.log(bird.body.velocity.y)
+  // 새의 위치 X가 캔버스의 너비와 같거나 더 크다면,왼쪽으로 돌아간다.
+  // 그리고 새의 위치 x가 0보다 작거나 같으면 오른쪽으로 다시 이동한다.
+  if( bird.x >= config.width - bird.width) { // - bird.width 를 해주는건 스프라이트가 마지막에 화면 밖으로 나가기 떄문 
+    bird.body.velocity.x = -VELOCITY;
+  } else if ( bird.x <=0 ) {
+    bird.body.velocity.x = VELOCITY;
+  }
 
 }
 
