1d70eacb1019bad6d6764dee246c2899282f6a03
dhk939393 <dhk9309@gmail.com>
2024-12-11 00:25:28 +0900

    리팩토링

--- flappy-bird/phaser-webpack-boilerplate-new-main/src/index.js
+++ flappy-bird/phaser-webpack-boilerplate-new-main/src/index.js
@@ -1,132 +1,26 @@
-import Phaser from "phaser";
-import PlayScene from "./scenes/PlayScene";
-
+import Phaser from 'phaser';
+import PlayScene from './scenes/PlayScene';
 
 const WIDTH = 800;
 const HEIGHT = 600;
-const BIRD_POSITION = {x: WIDTH * 0.1, y: HEIGHT /2 };
+const BIRD_POSITION = {x: WIDTH * 0.1, y: HEIGHT / 2 };
 
 const SHARED_CONFIG = {
-  width : WIDTH,
-  height : HEIGHT,
-  startPosition : BIRD_POSITION
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION
 }
 
 const config = {
-  // WebGL (Web graphics library) JS Api for rendering 2D and 3D graphics
   type: Phaser.AUTO,
   ...SHARED_CONFIG,
   physics: {
-    // Arcade physics plugin, manages physics simulation
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       debug: true,
-    },
+    }
   },
   scene: [new PlayScene(SHARED_CONFIG)]
-};
-
-const VELOCITY = 200;
-const PIPES_TO_RENDER = 4;
-
-let bird = null;
-let pipes = null;
-
-let pipeHorizontalDistance = 0;
-
-const pipeVerticalDistanceRange = [150, 250];
-const pipeHorizontalDistanceRange = [500, 550];
-
-const flapVelocity = 250;
-const initalBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
-
-function preload() {
-  this.load.image("pipe", "assets/pipe.png");
-}
-
-function create() {
-  pipes = this.physics.add.group();
-
-  for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    const upperPipe = pipes.create(0, 0, "pipe").setOrigin(0, 1);
-    const lowerPipe = pipes.create(0, 0, "pipe").setOrigin(0, 0);
-
-    placePipe(upperPipe, lowerPipe);
-  }
-
-  pipes.setVelocityX(-200);
-
-  this.input.on("pointerdown", flap);
-  this.spaceKey = this.input.keyboard.addKey(
-    Phaser.Input.Keyboard.KeyCodes.SPACE
-  );
-}
-
-// if bird y position is small than 0 or greater than height of the canvas
-// then alert "you have lost"
-function update(time, delta) {
-  if (bird.y > config.height || bird.y < -bird.height) {
-    restartBirdPosition();
-  }
-
-  // 스페이스바가 눌렸는지 확인
-  if (this.spaceKey.isDown) {
-    flap();
-  }
-
-  recyclePipes();
-}
-
-function placePipe(uPipe, lPipe) {
-  const rightMostX = getRightMostPipe();
-  const pipeVerticalDistance = Phaser.Math.Between(
-    ...pipeVerticalDistanceRange
-  );
-  const pipeVerticalPosition = Phaser.Math.Between(
-    0 + 20,
-    config.height - 20 - pipeVerticalDistance
-  );
-  const pipeHorizontalDistance = Phaser.Math.Between(
-    ...pipeHorizontalDistanceRange
-  );
-
-  uPipe.x = rightMostX + pipeHorizontalDistance;
-  uPipe.y = pipeVerticalPosition;
-
-  lPipe.x = uPipe.x;
-  lPipe.y = uPipe.y + pipeVerticalDistance;
-}
-
-function recyclePipes() {
-  const tempPipes = [];
-  pipes.getChildren().forEach(pipe => {
-    if (pipe.getBounds().right <= 0) {
-      tempPipes.push(pipe);
-      if ( tempPipes.length === 2) {
-        placePipe(...tempPipes);
-      }
-    }
-  })
-}
-
-function getRightMostPipe() {
-  let rightMostX = 0;
-
-  pipes.getChildren().forEach(function (pipe) {
-    rightMostX = Math.max(pipe.x, rightMostX);
-  });
-
-  return rightMostX;
-}
-
-function restartBirdPosition() {
-  bird.x = initalBirdPosition.x;
-  bird.y = initalBirdPosition.y;
-  bird.body.velocity.y = 0;
-}
-
-function flap() {
-  bird.body.velocity.y = -flapVelocity;
 }
 
 new Phaser.Game(config);
