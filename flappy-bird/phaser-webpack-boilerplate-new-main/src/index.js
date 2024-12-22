d305c3540c7c240e271949abe0804485658ed8ce
dhk939393 <dhk9309@gmail.com>
2024-12-22 12:53:23 +0900

    메뉴씬 + 최적화

--- flappy-bird/phaser-webpack-boilerplate-new-main/src/index.js
+++ flappy-bird/phaser-webpack-boilerplate-new-main/src/index.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import PlayScene from './scenes/PlayScene';
 import MenuScene from './scenes/MenuScene';
+import PreloadScene from './scenes/PreloadScene';
 
 const WIDTH = 800;
 const HEIGHT = 600;
@@ -12,6 +13,12 @@ const SHARED_CONFIG = {
   startPosition: BIRD_POSITION
 }
 
+const Scenes = [PreloadScene, MenuScene, PlayScene];
+const createScene = Scenes => new Scenes(SHARED_CONFIG);
+
+const initScenes = () => Scenes.map(createScene);
+
+
 const config = {
   type: Phaser.AUTO,
   ...SHARED_CONFIG,
@@ -21,7 +28,7 @@ const config = {
       debug: true,
     }
   },
-  scene: [new MenuScene(SHARED_CONFIG), new PlayScene(SHARED_CONFIG)]
+  scene: initScenes()
 }
 
 new Phaser.Game(config);
