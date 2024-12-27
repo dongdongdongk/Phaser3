8fd4e04143d8a5ce0a98606030c4526a579da104
dhk939393 <dhk9309@gmail.com>
2024-12-27 23:42:17 +0900

    일시정지 기능 추가

--- flappy-bird/phaser-webpack-boilerplate-new-main/src/scenes/PlayScene.js
+++ flappy-bird/phaser-webpack-boilerplate-new-main/src/scenes/PlayScene.js
@@ -28,11 +28,35 @@ class PlayScene extends BaseScene {
     this.createScore();
     this.createColliders();
     this.createPause();
+    this.listenToEvents();
   }
 
   update() {
     this.checkGameStatus();
     this.recyclePipes();
+  }
+
+  listenToEvents() {
+    this.events.on('resume', () => {
+      this.initialTime = 3;
+      this.countDownText = this.add.text(...this.screenCenter, 'Fly in: ' + this.initialTime, this.fontOptions).setOrigin(0.5);
+      this.timedEvent = this.time.addEvent({
+        delay: 1000,
+        callback: this.countDown,
+        callbackScope: this,
+        loop: true
+      })
+    })
+  }
+
+  countDown() {
+    this.initialTime --;
+    this.countDownText.setText('Fly in: ' + this.initialTime)
+    if(this.initialTime <= 0) {
+      this.countDownText.setText('');
+      this.physics.resume();
+      this.timedEvent.remove();
+    }
   }
 
 
