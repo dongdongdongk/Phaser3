cff92ea78381167b1120a58402d90ccd8423e095
dhk939393 <dhk9309@gmail.com>
2025-01-02 04:11:39 +0900

    dino플레이어 셋팅

--- dino/phaser3-typescript-simple-template-main/src/scenes/PlayScene.ts
+++ dino/phaser3-typescript-simple-template-main/src/scenes/PlayScene.ts
@@ -1,16 +1,26 @@
 import Phaser from "phaser";
 
 class PlayScene extends Phaser.Scene {
+    get gameHeight() {
+        return this.game.config.height as number;
+    }
+
     constructor() {
         super("PlayScene");
     }
 
     create() {
-        this.add
-        .tileSprite(0, this.game.config.height as number, 88, 26, 'ground')
-        .setOrigin(0, 1);
+        this.createEnvironment();
+        this.createPlayer();
+    }
+
+    createPlayer() {
+        this.physics.add.sprite(0, this.gameHeight, "dino-idle").setOrigin(0, 1);
+    }
+
+    createEnvironment() {
+        this.add.tileSprite(0, this.gameHeight, 88, 26, "ground").setOrigin(0, 1);
     }
 }
-
 
 export default PlayScene;
