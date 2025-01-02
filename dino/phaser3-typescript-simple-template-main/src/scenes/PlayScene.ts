c73c27f9502723ce382e186a0a670f1d86923431
dhk939393 <dhk9309@gmail.com>
2025-01-03 00:53:09 +0900

    타입스크립트

--- dino/phaser3-typescript-simple-template-main/src/scenes/PlayScene.ts
+++ dino/phaser3-typescript-simple-template-main/src/scenes/PlayScene.ts
@@ -1,6 +1,10 @@
 import Phaser from "phaser";
+import { SpriteWithDynamicBody } from "../types";
 
 class PlayScene extends Phaser.Scene {
+
+    player: SpriteWithDynamicBody;
+
     get gameHeight() {
         return this.game.config.height as number;
     }
@@ -12,14 +16,23 @@ class PlayScene extends Phaser.Scene {
     create() {
         this.createEnvironment();
         this.createPlayer();
+        this.registerPlayerControl();
     }
 
     createPlayer() {
-        this.physics.add.sprite(0, this.gameHeight, "dino-idle").setOrigin(0, 1);
+      this.player = this.physics.add.sprite(0, this.gameHeight, "dino-idle").setOrigin(0, 1);
     }
 
     createEnvironment() {
         this.add.tileSprite(0, this.gameHeight, 88, 26, "ground").setOrigin(0, 1);
+    }
+
+    registerPlayerControl(){
+        const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        spaceBar.on('down', () => {
+            this.player.setVelocityY(-1600);
+        });
     }
 }
 
