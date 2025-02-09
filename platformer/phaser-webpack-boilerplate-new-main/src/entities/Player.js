b50ed5269768f0609a6352de54f87f12c3b44973
dhk939393 <dhk9309@gmail.com>
2025-02-10 01:22:32 +0900

    캐릭터 런 애니메이션 추가

--- platformer/phaser-webpack-boilerplate-new-main/src/entities/Player.js
+++ platformer/phaser-webpack-boilerplate-new-main/src/entities/Player.js
@@ -8,6 +8,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         scene.physics.add.existing(this);
 
         this.init();
+        this.initEvents();
     }
 
     init() {
@@ -17,11 +18,19 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.body.setGravityY(500);
         this.setCollideWorldBounds(true);
         this.cursors = this.scene.input.keyboard.createCursorKeys();
+
+        this.scene.anims.create({
+            key: 'run',
+            frames: this.scene.anims.generateFrameNumbers('player', { start: 11, end: 16, frameRate: 10, repeat: -1 }),
+        });
     }
 
-    preUpdate() {
-        const { left, right, space, up } = this.cursors;
+    initEvents() {
+        this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
+    }
 
+    update() {
+        const { left, right, space, up } = this.cursors;
         if(left.isDown) {
             this.setVelocityX(-this.playerSpeed);
         } else if (right.isDown) {
@@ -29,7 +38,11 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         } else {
             this.setVelocityX(0);
         }
+        //dont play it again if it is already playing
+        // ignoreIfPlaying: true 
+        this.play('run', true, { ignoreIfPlaying: true });
     }
+
 }
 
 
